12e49600d9f8e50965fd6e6e125c63f018be5811
EKiwsant <kiwsanthia@gmail.com>
2019-12-11 19:06:50 +0700

    improved seo concerns

--- controllers/homeController.go
+++ controllers/homeController.go
@@ -3,9 +3,22 @@ package controllers
 import (
 	"github.com/kiwsan/io/utils"
 	"net/http"
+	"strings"
 )
 
 func HomeGetHandler(w http.ResponseWriter, r *http.Request) {
+
+	e := "SomeKey describing content - eg checksum"
+	w.Header().Set("Etag", e)
+	w.Header().Set("Cache-Control", "max-age=2592000") // 30 days
+
+	if match := r.Header.Get("If-None-Match"); match != "" {
+		if strings.Contains(match, e) {
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+	}
+
 	utils.ExecuteTemplate(w, "index.html", nil)
 }
 
