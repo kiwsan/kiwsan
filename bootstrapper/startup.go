ad2c4aa4c28e2997a4f6fd5fc1a512a1ab4f30ea
EKiwsant <kiwsanthia@gmail.com>
2019-12-11 21:59:25 +0700

    improved seo concerns

--- bootstrapper/startup.go
+++ bootstrapper/startup.go
@@ -1,6 +1,8 @@
 package bootstrapper
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/kiwsan/io/controllers"
 	"github.com/kiwsan/io/routes"
 	"github.com/kiwsan/io/utils"
@@ -8,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"time"
 )
 
 func OnStart() {
@@ -21,10 +24,6 @@ func initRoutes() {
 
 	r := routes.NewRouter()
 
-	// handle static files
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
-		CacheControlWrapper(http.FileServer(http.Dir("/static/")))))
-
 	r.NotFoundHandler = http.HandlerFunc(controllers.HomeGetHandler) // work around for SPA serving index.html
 
 	handler := cors.Default().Handler(r)
@@ -33,8 +32,14 @@ func initRoutes() {
 }
 
 func CacheControlWrapper(h http.Handler) http.Handler {
+	data := []byte(time.Now().String())
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "max-age=3600")
+		w.Header().Set("Cache-Control", "max-age=2592000") // 30 days
+
+		etag := fmt.Sprintf("%x", md5.Sum(data))
+		w.Header().Set("Etag", etag)
+
 		h.ServeHTTP(w, r)
 	})
 }
@@ -48,13 +53,13 @@ func initStatics() {
 
 	// This is the only way I have found to be able to serve images requested in the templates
 	http.Handle("/static/images/", http.StripPrefix("/static/images/",
-		http.FileServer(http.Dir(path.Join(rootDir, "/static/images/")))))
+		CacheControlWrapper(http.FileServer(http.Dir(path.Join(rootDir, "/static/images/"))))))
 
 	http.Handle("/static/css/", http.StripPrefix("/static/css/",
-		http.FileServer(http.Dir(path.Join(rootDir, "/static/css/")))))
+		CacheControlWrapper(http.FileServer(http.Dir(path.Join(rootDir, "/static/css/"))))))
 
 	http.Handle("/static/js/", http.StripPrefix("/static/js/",
-		http.FileServer(http.Dir(path.Join(rootDir, "/static/js/")))))
+		CacheControlWrapper(http.FileServer(http.Dir(path.Join(rootDir, "/static/js/"))))))
 
 	utils.LoadTemplates("templates/*.html")
 
