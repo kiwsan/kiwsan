b824427b61c92b20e728f8ab37ce7d93af31232d
kiwsan, inc <kiwsanthia@gmail.com>
2020-08-27 18:31:06 +0700

    migrate to vue 3.0 preview

--- README.md
+++ README.md
@@ -1,32 +1,4 @@
-# kiwsan, inc.
-
-## Quick start
-
-Build using docker command:
-
-```bash
-$ docker build -f Dockerfile -t vuejsapp . 
-```
-
-Run using docker command:
-
-```bash
-$ docker run --rm -it -p 8088:8080 vuejsapp
-```
-
-Build and run using docker-compose:
-
-```bash
-$ docker-compose up -d
-```
-
-```bash
-$ curl http://localhost:8088
-```
-
-## On Mobile screen
-
-<img src="https://raw.githubusercontent.com/kiwsan/kiwsan/master/screenshots/iPhone%20XR%2C%20XS%20Max%2C%2011%20%E2%80%93%201.png" alt="dasktop" width="550"/>
+# kiwsan
 
 ## Project setup
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,18 @@
 <template>
-  <v-content class="home">
-    <vue-headful
-      title="kiwsan, inc."
-      description="Hi, I’m Ekkchai. Nice to meet you. Since beginning my journey as a web developer nearly 4 years ago, I've done remote work for agencies, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually improving my chops."
-      keywords="Portfolio, Web Developer, Bangkok, Thailand"
-      image="https://kiwsan.com/img/iam-nothing-like-you.264911da.jpg"
-      url="https://kiwsan.com/"
-    />
-    <WebDeveloper />
-    <WhatIDo />
-    <WhoIAm />
-    <ShowCase />
-    <WhyHireMe />
-    <Certifications />
-    <ContactMe />
-    <Framework />
-    <Footer />
-  </v-content>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import WebDeveloper from "@/components/WebDeveloper";
-import WhatIDo from "@/components/WhatIDo";
-import WhoIAm from "@/components/WhoIAm";
-import ShowCase from "@/components/ShowCase";
-import WhyHireMe from "@/components/WhyHireMe";
-import Certifications from "@/components/Certifications";
-import ContactMe from "@/components/ContactMe";
-import Framework from "@/components/Framework";
-import Footer from "@/components/Footer";
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-export default {
+export default defineComponent({
   name: "Home",
   components: {
-    WebDeveloper,
-    WhatIDo,
-    WhoIAm,
-    ShowCase,
-    WhyHireMe,
-    Certifications,
-    ContactMe,
-    Framework,
-    Footer
-  }
-};
+    HelloWorld,
+  },
+});
 </script>
