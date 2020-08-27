7adf3d5e0b5cf32105771623d640baa74126270e
kiwsan, inc <kiwsanthia@gmail.com>
2020-08-27 21:49:35 +0700

    added bootstrap-vue and font-awesome-icon

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import createRouter from './router'
-import createStore from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import createRouter from "./router";
+import createStore from "./store";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
+Vue.config.productionTip = false;
 
 export default () => {
-  const store = createStore()
-  const router = createRouter()
+  const store = createStore();
+  const router = createRouter();
   return new Vue({
     router,
     store,
-    render: h => h(App)
-  })
-}
+    render: (h) => h(App),
+  });
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <b-col lg="4" class="pb-2"
+      ><b-button size="sm">Small Button <font-awesome-icon :icon="['fas', 'user-secret']" />
+</b-button></b-col
+    >
   </div>
 </template>
 
