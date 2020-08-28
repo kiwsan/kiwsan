2a9e31a2f095a628910415a45a14926953e42957
kiwsan <kiwsanthia@gmail.com>
2020-08-28 09:33:48 +0700

    added axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,29 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <b-col lg="4" class="pb-2"
-      ><b-button size="sm">Small Button <font-awesome-icon :icon="['fas', 'user-secret']" />
-</b-button></b-col
-    >
+    <b-col lg="4" class="pb-2">
+      <b-button size="sm">
+        Small Button
+        <font-awesome-icon :icon="['fas', 'user-secret']" />
+      </b-button>
+    </b-col>
+    {{posts}}
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+<script>
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-@Component({
+import axios from "axios";
+export default {
+  name: "Home",
   components: {
     HelloWorld,
   },
-})
-export default class Home extends Vue {}
+  async asyncData() {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/1`
+    );
+    return { posts: response.data };
+  },
+};
 </script>
